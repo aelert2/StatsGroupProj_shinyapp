d11efd050cd6eaa6d913ab214e04197a6e371d80
aelert2 <aelert2@wisc.edu>
2020-04-30 23:39:30 -0500

    Amanda's updates

--- app.R
+++ app.R
@@ -1,4 +1,5 @@
 # Example from https://shiny.rstudio.com/ 
+# Navbar example: https://shiny.rstudio.com/gallery/navbar-example.html
 
 # Load packages
 library(shiny)
@@ -15,174 +16,91 @@ wpa_shifts <- read_csv("wpa_shifts.csv")
 
 
 # Define UI
-ui <- fluidPage(theme = shinytheme("united"),
-                titlePanel("NFL Post-TD Plays: Win Probability Shifts after XPT or 2PT"),
-                
-                # Project description
-                fluidRow(
-                  column(width = 12, 
-                         style = 'padding:1em;',
-                         includeMarkdown("desc_text.md")
-                  )
-                ),
-                br(),
-                br(),
-                br(),
-                
-                fluidRow(
-                  
-                  # XPT graph
-                  column(width = 6,
-                         style = 'padding:1em;',
-                         sidebarLayout(
-                           sidebarPanel(
-                             
-                             # Chose what post-td play results you want to see
-                             checkboxGroupInput("extra_point_result_options", "XPT Result:",
-                                                c("Good" = "good",
-                                                  "Failed" = "failed",
-                                                  "Blocked" = "blocked",
-                                                  "aborted" = "aborted"))
-                           ),
-                           
-                           # Output: Linegraph
-                           mainPanel(
-                             plotOutput(outputId = "linegraphXPT")
-                           )
-                         ),
-                  ),
-                  
-                  # 2PT graph
-                  column(width = 6,
-                         style = 'padding:1em;',
-                         sidebarLayout(
-                           sidebarPanel(
-                             
-                             # Chose what years to show
-                             # sliderInput("year_range", "Year Range:",
-                             #             min = 2009, max = 2018, 
-                             #             value = c(2012,2016)),
-                             # numericInput(inputId = "year_lb",
-                             #              label = "First year of data to gather:",
-                             #              value = 2009,
-                             #              min = 2009, max = 2018),
-                             # numericInput(inputId = "year_ub",
-                             #              label = "Last year of data to gather:",
-                             #              value = 2018,
-                             #              min = 2009, max = 2018),
-                             
-                             # Chose what post-td play results you want to see
-                             checkboxGroupInput("two_point_conv_result_options", "2PT Conv. Result:",
-                                                c("Success" = "success",
-                                                  "Failure" = "failure"))
-                           ),
-                           
-                           # Output: Linegraph
-                           mainPanel(
-                             plotOutput(outputId = "linegraph2PT")
-                           )
-                         ),
-                  )
-                ),
-                
-                br(),
-                br(),
-                br(),
-                br(),
-                
-                fluidRow(
-                  column(width = 12,
-                         style = 'padding:1em;',
-                         sidebarLayout(
-                           # Sidebar panel for inputs ----
-                           sidebarPanel(
-                             
-                             # Input: Select the random distribution type ----
-                             checkboxGroupInput("extra_point_type_options", "Post-TD Play Type:",
-                                                c("Kick" = "Kick",
-                                                  "Two-point Conversion" = "Two-PointConversion")),
-                             
-                             # br() element to introduce extra vertical spacing ----
-                             br(),
-                             
-                             # Input: Slider for the number of observations to generate ----
-                             sliderInput("game_minutes_remaining",
-                                         "Game Minutes Remaining:",
-                                         value = 15, # start of 4th quarter
-                                         min = 0,
-                                         max = 60)
-                           ),
-                           
-                           
-                           # Output: Description, lineplot, and reference
-                           mainPanel(
-                             plotOutput(outputId = "scatterplot")
-                           )
-                         )
-                  )
-                ),
-                
-                br(),
-                br(),
-                br(),
-                br(),
-                
-                fluidRow(
-                  column(width = 12,
-                         style = 'padding:1em;',
-                         sidebarLayout(
-                           # Sidebar panel for inputs ----
-                           sidebarPanel(
-                             
-                             # Input: Select the random distribution type ----
-                             checkboxGroupInput("play_type", "Post-TD Play Type:",
-                                                c("Kick" = "kick",
-                                                  "Two-point Conversion" = "two_pt_conv")),
-                             ),
-                           
-                           
-                           # Output: Description, lineplot, and reference
-                           mainPanel(
-                             plotOutput(outputId = "linegraphWPAShifts")
-                             )
-                           )
-                         )
-                  )
+ui <- navbarPage("2-point Conversions in the NFL",
+                 tabPanel("Background",
+                          includeMarkdown("desc_text.md")
+                 ),
+                 tabPanel("Exploration",
+                          fluidRow(
+                            column(width = 12, 
+                                   style = 'padding:1em;',
+                                   sidebarLayout(
+                                     sidebarPanel(
+                                       # Chose what post-td play results you want to see
+                                       checkboxGroupInput("prop_options", "Post-TD Play Type:",
+                                                          c("Kick" = "prop_expt",
+                                                            "Two-point Conversion" = "prop_twopt"))
+                                       ),
+                                     mainPanel(
+                                       plotOutput(outputId = "bargraph")
+                                       )
+                                     )
+                                   )
+                            ),
+                          
+                          fluidRow(
+                            column(width = 12, 
+                                   style = 'padding:1em;',
+                                   sidebarLayout(
+                                     sidebarPanel(
+                                       checkboxGroupInput("extra_point_type_options", "Post-TD Play Type:",
+                                                         c("Kick" = "Kick",
+                                                           "Two-point Conversion" = "Two-PointConversion")),
+                                       br(),
+                                       sliderInput("game_minutes_remaining",
+                                                  "Game Minutes Remaining:",
+                                                  value = 15, # start of 4th quarter
+                                                  min = 0,
+                                                  max = 60)
+                                       ),
+                                     
+                                     mainPanel(
+                                       plotOutput(outputId = "scatterplot")
+                                       )
+                                     )
+                                   )
+                          )
+                       ),
+                 
+                 tabPanel("Logistic Model",
+                          sidebarLayout(
+                            sidebarPanel(
+                              checkboxGroupInput("play_type", "Post-TD Play Type:",
+                                                 c("Kick" = "kick",
+                                                   "Two-point Conversion" = "two_pt_conv")),
+                            ),
+                            
+                            mainPanel(
+                              plotOutput(outputId = "linegraphWPAShifts")
+                            )
+                          )
+                 ),
+                 tabPanel("Conclusion")
 )
 
 
-# Define server function
+
 server <- function(input, output) {
   
-  # Create linegraph of frequency of XPT conv from 2009-2018
-  output$linegraphXPT <- renderPlot({
+  # Create bar graph of proportion of post-td plays from 2009-2018
+  output$bargraph <- renderPlot({
     post_td_plays %>% 
-      filter(extra_point_attempt == 1) %>% 
-      filter(extra_point_result %in% input$extra_point_result_options) %>%
-      group_by(year, extra_point_result) %>% 
+      group_by(year, extra_point_type) %>% 
       summarise(n = n()) %>% 
-      ggplot(aes(x = year, y = n, group = extra_point_result)) +
-      geom_line(aes(color = extra_point_result)) +
-      geom_point(aes(color = extra_point_result)) +
+      pivot_wider(names_from = "extra_point_type", values_from = "n") %>% 
+      rename(ex_pt_attempt = Kick,
+             two_pt_attempt = `Two-PointConversion`) %>% 
+      mutate(prop_expt = ex_pt_attempt / (ex_pt_attempt + two_pt_attempt),
+             prop_twopt = two_pt_attempt / (ex_pt_attempt + two_pt_attempt)) %>% 
+      pivot_longer(cols = c('prop_expt', 'prop_twopt'), names_to = 'play_type2', values_to = "proportion") %>% 
+      filter(play_type2 %in% input$prop_options) %>% 
+      ggplot(aes(x = year, y = proportion, fill = play_type2)) +
+      geom_bar(stat='identity', position='dodge') +
       geom_vline(xintercept = 2015, linetype="dashed", color = "darkgray", size = 1) +
-      labs(title="Extra Points in the NFL from 2009 to 2018", x="Year", y = "Count")
+      labs(title="All Post-TD Plays in the NFL from 2009 to 2018", x="Year", y = "Proportion") +
+      guides(fill = guide_legend(title=NULL))
   })
-  
-  # Create linegraph of frequency of 2PT conv from 2009-2018
-  output$linegraph2PT <- renderPlot({
-    post_td_plays %>%
-      #filter(two_point_attempt == 1) %>%
-      #filter(year >= input$year_lb & year <= input$year_ub) %>%
-      #filter(year >= input$year_range[1] & year <= input$year_range[2]) %>%
-      filter(two_point_conv_result %in% input$two_point_conv_result_options) %>%
-      group_by(year, two_point_conv_result) %>%
-      summarise(n = n()) %>%
-      ggplot(aes(x = year, y = n, group = two_point_conv_result)) +
-      geom_line(aes(color = two_point_conv_result)) +
-      geom_point(aes(color = two_point_conv_result)) +
-      geom_vline(xintercept = 2015, linetype="dashed", color = "darkgray", size = 1) +
-      labs(title="2-pt Conversions in the NFL from 2009 to 2018",x="Year", y = "Count")
-  })
+
   
   # Create scatterplot object the plotOutput function is expecting
   output$scatterplot <- renderPlot({
